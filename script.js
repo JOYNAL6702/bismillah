82576e2c3adc050fd9052f17bf5c803820a79bfe
Joynal <joynal6702@gmail.com>
2024-01-20 01:58:32 -0800

    changes

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*let name;
+name = "joynal";
+
+alert(name);
+
+
+let school = "chatra";
+
+let school = true;//false
+let name = null;*/
+
+/*let name = prompt ("inter your name");
+console.log(name);
+console.log(typeof name);*/
+
+/*let firstName=prompt("inter your first name");
+let lastName=prompt("inter your last name");
+alert(`hello ${firstName} ${lastName}`)*/
+
+/*let isAdmin = confirm("Are you admin");
+console.log(isAdmin);*/
+
+
